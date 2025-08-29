ccbd73baa2129d7e82156be8db24d280f3803d2e
Josue <nmartel@fi.uba.ar>
2025-08-29 03:36:52 -0300

    Agrego parametros de nc para cerrar luego de recibir el EOF

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,7 +4,7 @@ MSG="hola-echo"
 
 docker build -t validator-latest ./validator
 
-RESPONSE=$(docker run --rm --network tp0_testing_net --entrypoint /bin/sh validator-latest -c "echo '$MSG' | nc server 12345")
+RESPONSE=$(docker run --rm --network tp0_testing_net --entrypoint /bin/sh validator-latest -c "echo '$MSG' | nc -q 0 server 12345")
 
 if [ "$RESPONSE" = "$MSG" ]; then
   echo "action: test_echo_server | result: success"
